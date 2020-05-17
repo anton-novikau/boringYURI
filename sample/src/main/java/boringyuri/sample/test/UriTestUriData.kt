ada583cca50b186ac4b0bdce5c5b135bb88f4ee7
Anton Novikau <av.novikov@gmail.com>
2020-05-17 23:50:13 +0300

    Add test samples to verify the cases of type conversion
    - Add test `Uri` factory verify the maximum cases of possible type
    conversions from object to string and conversion from string to object
    for associated data classes.
    - Add test `UriData` interfaces to verify correct conversion from string
    to object for independent data classes.

---
+++ sample/src/main/java/boringyuri/sample/test/UriTestUriData.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 Anton Novikau
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package boringyuri.sample.test
+
+import android.net.Uri
+import boringyuri.api.DefaultValue
+import boringyuri.api.Param
+import boringyuri.api.Path
+import boringyuri.api.UriData
+
+@UriData("/path/segment/{uri}")
+interface UriTestUriData {
+
+    @Path("uri")
+    fun getUriSegment(): Uri
+
+    @Param
+    fun getNullableParam(): Uri?
+
+    @Param
+    fun getNonNullParam(): Uri
+
+    @Param
+    @DefaultValue("https://example.com/uri")
+    fun getNonNullWithDefaultParam(): Uri
+
+}
---
+++ sample/src/main/java/boringyuri/sample/test/UserTestUriData.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 Anton Novikau
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package boringyuri.sample.test
+
+import boringyuri.api.DefaultValue
+import boringyuri.api.Param
+import boringyuri.api.Path
+import boringyuri.api.UriData
+import boringyuri.sample.data.User
+
+@UriData("/path/segment/{user}")
+interface UserTestUriData {
+
+    @Path("user")
+    fun getUserSegment(): User
+
+    @Param
+    fun getNullableParam(): User?
+
+    @Param
+    fun getNonNullParam(): User
+
+    @Param
+    @DefaultValue("42;John Doe")
+    fun getNonNullWithDefaultParam(): User
+
+}
